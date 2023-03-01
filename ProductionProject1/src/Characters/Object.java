f536cad3f27c60e3e30cc7cb98b5b0e167848d93
Lucas-M1 <lucasmiller171101@gmail.com>
2023-03-01 23:47:53 +0000

    Added custom created sprite character
    Moves in all 4 directions based on which direction the user is
    travelling (left, right, up, down).

--- ProductionProject1/src/Characters/Object.java
+++ ProductionProject1/src/Characters/Object.java
@@ -1,10 +1,18 @@
 package Characters;
 
+import java.awt.image.BufferedImage;
+
 public class Object {
 	
 	public int posX;
 	public int posY;
 	public int playerSpeed;
+	
+	public String direction;
+	public BufferedImage l1, l2, l3, upStill, downStill, d1, d2, d3, d4, r1, r2, r3, u1, u2, u3;
+	
+	public int playerCounter;
+	public int playerNumber;
 	
 
 }
--- ProductionProject1/src/Characters/Player.java
+++ ProductionProject1/src/Characters/Player.java
@@ -2,6 +2,10 @@ package Characters;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import main.Frame;
 import main.UserInput;
@@ -13,8 +17,33 @@ public class Player extends Object {
 	
 	public Player(Frame f, UserInput k) {
 		basePlayerStats();
+		playerImage();
 		this.f = f;
 		this.k = k;
+	}
+	
+	public void playerImage() {
+		
+		try {
+			l1 = ImageIO.read(getClass().getResourceAsStream("/character/sprite-1.png"));
+			l2 = ImageIO.read(getClass().getResourceAsStream("/character/sprite-2.png"));
+			l3 = ImageIO.read(getClass().getResourceAsStream("/character/sprite-3.png"));
+			upStill = ImageIO.read(getClass().getResourceAsStream("/character/sprite-4.png"));
+			downStill = ImageIO.read(getClass().getResourceAsStream("/character/sprite-6.png"));
+			u1 = ImageIO.read(getClass().getResourceAsStream("/character/sprite-5.png"));
+			u2 = ImageIO.read(getClass().getResourceAsStream("/character/sprite-8.png"));
+			u3 = ImageIO.read(getClass().getResourceAsStream("/character/sprite-9.png"));
+			d1 = ImageIO.read(getClass().getResourceAsStream("/character/sprite-7.png"));
+			d2 = ImageIO.read(getClass().getResourceAsStream("/character/sprite-10.png"));
+			d3 = ImageIO.read(getClass().getResourceAsStream("/character/sprite-11.png"));
+			r1 = ImageIO.read(getClass().getResourceAsStream("/character/sprite-12.png"));
+			r2 = ImageIO.read(getClass().getResourceAsStream("/character/sprite-13.png"));
+			r3 = ImageIO.read(getClass().getResourceAsStream("/character/sprite-14.png"));
+					
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void basePlayerStats() {
@@ -22,21 +51,26 @@ public class Player extends Object {
 		posX = 150;
 		posY = 150;
 		playerSpeed = 4;
+		direction = "right";
 		
 	}
 	
 	public void refresh() {
 		if (k.up == true) {
+			direction = "up";
 			posY = posY - playerSpeed;
 			
 		}
 		else if(k.down == true) {
+			direction = "down";
 			posY = posY + playerSpeed;
 		}
 		else if (k.left == true) {
+			direction = "left";
 			posX = posX - playerSpeed;
 		}
 		else if (k.right == true) {
+			direction = "right";
 			posX = posX + playerSpeed;
 		}
 		
@@ -44,9 +78,24 @@ public class Player extends Object {
 	
 	public void draw(Graphics2D graphics2d) {
 		
-		graphics2d.setColor(Color.green);
-		graphics2d.fillRect(posX, posY, f.realTileSize, f.realTileSize);
-		
+		//graphics2d.setColor(Color.green);
+		//graphics2d.fillRect(posX, posY, f.realTileSize, f.realTileSize);
+		BufferedImage img = null;
+		switch (direction) {
+		case "right":
+			img = r1;
+			break;
+		case "down":
+			img = d1;
+			break;
+		case "up":
+			img = u1;
+			break;
+		case "left":
+			img = l1;
+			break;
+		}
+		graphics2d.drawImage(img, posX, posY, f.realTileSize, f.realTileSize, null);
 	}
 
 }
